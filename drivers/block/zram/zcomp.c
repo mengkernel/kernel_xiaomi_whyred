d73a910c87cfe38cbef311ad68ae84a4f8bfda11
Diaz1401 <diaznura@outlook.com>
2025-07-20 10:08:19 +0700

    zram: Use Zstd by default
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -23,6 +23,9 @@ static const char * const backends[] = {
 	"lzo",
 	"lzo-rle",
 #endif
+#if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
+	"zstd",
+#endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
 #endif
@@ -31,9 +34,6 @@ static const char * const backends[] = {
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_842)
 	"842",
-#endif
-#if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
-	"zstd",
 #endif
 };
 
